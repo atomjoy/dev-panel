00267333e23a11a384e6ec4a7bddacc51d699073
Atomjoy <atomjoy.official@gmail.com>
2024-05-22 17:13:18 +0200

    Opengraph website

---
+++ resources/views/opengraph/og-website.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Strony internetowe Przasnysz | Web developer | Atomjoy</title>
+	<meta name="description" content="Tworzenie stron i sklepów internetowych, kodowanie szablonów email newslettera, modelowanie 3d i rendering 3d, projektowanie logo, strony internetowe Przasnysz." />
+	<meta name="keywords" content="strony internetowe Przasnysz, strony www Przasnysz, sklepy internetowe Przasnysz, projektowanie logo Przasnysz, strony internetowe, strony www, sklepy internetowe, szablony newsletter, psd do html, cięcie psd do html, psd to html, web developer portfolio" />
+	<link rel="canonical" href="https://atomjoy.pl/" />
+	<meta name="geo.position" content="53.017850;20.904640" />
+	<meta name="geo.placename" content="Przasnysz" />
+
+	<!-- Favicon -->
+	<link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-icon-57x57.png" />
+	<link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-icon-60x60.png" />
+	<link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-icon-72x72.png" />
+	<link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-icon-76x76.png" />
+	<link rel="apple-touch-icon" sizes="114x114" href="/favicon/apple-icon-114x114.png" />
+	<link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-icon-120x120.png" />
+	<link rel="apple-touch-icon" sizes="144x144" href="/favicon/apple-icon-144x144.png" />
+	<link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-icon-152x152.png" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-icon-180x180.png" />
+	<link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-icon-192x192.png" />
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+	<link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png" />
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+	<link rel="shortcut icon" href="/favicon/favicon.ico" type="image/x-icon" />
+	<link rel="icon" href="/favicon/favicon.ico" type="image/x-icon" />
+
+	<!-- OpenGraph -->
+	<meta property="og:url" content="https://atomjoy.pl/" />
+	<meta property="og:locale" content="pl_PL" />
+	<meta property="og:site_name" content="Atomjoy.pl" />
+	<meta property="og:title" content="Strony Internetowe Przasnysz | Atomjoy" />
+	<meta property="og:description" content="Tworzenie stron internetowych i sklepów internetowych, kodowanie stron produktów oraz szablonów e-mail newslettera w HTML, projektowanie logo, administracja serwerami VPS i hostingiem, identyfikacja wizualna, modelowanie 3D, rendering 3d, wizualizacje wnętrz, produktów i animacje 3d dla stron www." />
+	<meta property="og:image" content="https://atomjoy.pl/images/atomjoy.png" />
+	<meta property="og:image:width" content="450" />
+	<meta property="og:image:height" content="450" />
+	<meta property="og:type" content="website" />
+
+	<!-- Twitter -->
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:site" content="@atomjoys" />
+	<meta name="twitter:title" content="Strony internetowe Przasnysz | Atomjoy" />
+	<meta name="twitter:description" content="Tworzenie stron internetowych i sklepów internetowych, kodowanie stron produktów oraz szablonów e-mail newslettera w HTML, projektowanie logo, administracja serwerami VPS i hostingiem, identyfikacja wizualna, modelowanie 3D, rendering 3d, wizualizacje wnętrz, produktów i animacje 3d dla stron www." />
+	<meta name="twitter:image" content="https://atomjoy.pl/img/logo.png" />
+	<meta name="twitter:image:alt" content="Atomjoy" />
+
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-GRGTK8SYTV"></script>
+	<script>
+		window.dataLayer = window.dataLayer || []
+		function gtag() {
+			dataLayer.push(arguments)
+		}
+		gtag('consent', 'default', {
+			ad_user_data: 'granted',
+			ad_personalization: 'granted',
+			ad_storage: 'granted',
+			analytics_storage: 'granted',
+			// 'wait_for_update': 500,
+		})
+		gtag('js', new Date())
+		gtag('config', 'G-GRGTK8SYTV')
+	</script>
+
+	<link rel="stylesheet" href="style.css" />
+	<script src="main.js"></script>
+</head>
+
+<body>
+	Website opengraph https://ogp.me/
+</body>
+
+</html>
